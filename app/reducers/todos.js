862af4b537ff28fd02be5f35ce59fbfb2fd351a1
Thibaut Tiberghien <thibaut@planecq.com>
2017-02-05 11:12:57 +0000

    missing files

---
+++ app/reducers/todos.js
@@ -0,0 +1,35 @@
+const todo = (todo = {}, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+    return {
+      id: action.id,
+      text: action.text,
+      completed: false
+    }
+    case 'TOGGLE_TODO':
+    if (action.id !== todo.id) {
+      return todo
+    }
+    return Object.assign({}, todo, {
+      completed: !todo.completed
+    })
+    default:
+      todo
+  }
+}
+
+const todos = (todos = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+    return [
+      ...todos,
+      todo(undefined, action)
+    ]
+    case 'TOGGLE_TODO':
+    return todos.map(t => todo(t, action))
+    default:
+    return todos
+  }
+}
+
+export default todos
---
+++ app/reducers/visibilityFilter.js
@@ -0,0 +1,10 @@
+const visibilityFilter = (visibilityFilter = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+    return action.filter
+    default:
+    return visibilityFilter
+  }
+}
+
+export default visibilityFilter
